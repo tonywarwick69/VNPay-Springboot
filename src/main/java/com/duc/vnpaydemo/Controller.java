e293db2fabc6af665e78685682981ab9f187eab5
tonywarwick69 <duc.khang165@gmail.com>
2024-04-08 21:09:30 +0700

    First commit

---
+++ src/main/java/com/duc/vnpaydemo/Controller.java
@@ -0,0 +1,54 @@
+package com.duc.vnpaydemo;
+
+import com.duc.vnpaydemo.Config.VNPayService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+    @Autowired
+    private VNPayService vnPayService;
+
+
+    @GetMapping("")
+    public String home(){
+        return "index";
+    }
+
+    @PostMapping("/submitOrder")
+    public String submidOrder(@RequestParam("amount") int orderTotal,
+                            @RequestParam("orderInfo") String orderInfo,
+                            HttpServletRequest request){
+        String baseUrl = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort();
+        String vnpayUrl = vnPayService.createOrder(orderTotal, orderInfo, baseUrl);
+        return "redirect:" + vnpayUrl;
+    }
+
+    @GetMapping("/vnpay-payment")
+    public String GetMapping(HttpServletRequest request, Model model){
+        int paymentStatus =vnPayService.orderReturn(request);
+
+        String orderInfo = request.getParameter("vnp_OrderInfo");
+        String paymentTime = request.getParameter("vnp_PayDate");
+        String transactionId = request.getParameter("vnp_TransactionNo");
+        //Because VNPAY has rule:
+        /*
+        	Số tiền thanh toán. Số tiền không mang các ký tự phân tách thập phân, phần nghìn, ký tự tiền tệ.
+        	Để gửi số tiền thanh toán là 10,000 VND (mười nghìn VNĐ) thì merchant cần nhân thêm 100 lần (khử phần thập phân), sau đó gửi sang VNPAY là: 1000000
+        	In order to deal with incorrect total price return in correct on GUI but save correctly in database of VNPAY we must divided vnp_amount by 100
+         */
+        Double convertTotalPrice = Double.valueOf(request.getParameter("vnp_Amount"))/100;
+        String totalPrice = convertTotalPrice.toString();
+
+        model.addAttribute("orderId", orderInfo);
+        model.addAttribute("totalPrice", totalPrice);
+        model.addAttribute("paymentTime", paymentTime);
+        model.addAttribute("transactionId", transactionId);
+
+        return paymentStatus == 1 ? "ordersuccess" : "orderfail";
+    }
+}
